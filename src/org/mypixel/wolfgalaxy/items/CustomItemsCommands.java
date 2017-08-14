c5b118df33dcfdb5bdf1089f70231cf899d06620
Syrian2 <qusai061@gmail.com>
2017-08-14 18:18:20 +0400

    Removing listeners

--- src/org/mypixel/wolfgalaxy/items/CustomItemsCommands.java
+++ src/org/mypixel/wolfgalaxy/items/CustomItemsCommands.java
@@ -20,7 +20,7 @@ import org.mypixel.wolfgalaxy.Main;
  * Created by Albus on 12/08/2017.
  */
 
-public class CustomItemsCommands implements CommandExecutor, Listener {
+public class CustomItemsCommands implements CommandExecutor {
 
     public Main plugin;
 
@@ -28,10 +28,12 @@ public class CustomItemsCommands implements CommandExecutor, Listener {
         plugin = instance;
     }
 
+    @Override
+
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 
         if (!(sender instanceof Player)) {
-            return false;
+            return true;
         }
 
         Player player = (Player) sender;
@@ -53,41 +55,5 @@ public class CustomItemsCommands implements CommandExecutor, Listener {
         return true;
     }
 
-        @EventHandler
-
-        public void onInventoryClick (InventoryClickEvent event){
-            Player player = (Player) event.getWhoClicked();
-            ItemStack itemClicked = event.getCurrentItem();
-            Inventory inv = event.getInventory();
-            ItemMeta meta = itemClicked.getItemMeta();
-
-            if (inv.getName().equals(ChatColor.AQUA + "Custom Armour")) {
-                if (!(itemClicked == null || itemClicked.getType().equals(Material.AIR))) {
-                    for (String string : plugin.config.getConfigurationSection("Items").getKeys(false)) {
-                        if (ChatColor.translateAlternateColorCodes('&',
-                                plugin.config.getString("Items." + string + ".Name")).equals(meta.getDisplayName())
-                                && player.hasPermission("wg.items.wear." + string)) {
-
-                            if (plugin.config.getString("Items." + string + ".Type").equalsIgnoreCase("Helmet")) {
-                                player.getInventory().setHelmet(itemClicked);
-                            } else if (plugin.config.getString("Items." + string + ".Type").equalsIgnoreCase("Chestplate")) {
-                                player.getInventory().setChestplate(itemClicked);
-                            } else if (plugin.config.getString("Items." + string + ".Type").equalsIgnoreCase("Leggings")) {
-                                player.getInventory().setLeggings(itemClicked);
-                            } else if (plugin.config.getString("Items." + string + ".Type").equalsIgnoreCase("Boots")) {
-                                player.getInventory().setBoots(itemClicked);
-                            }
-                        }
-                    }
-                }
-            } else if (inv == player.getInventory()) {
-                for (String string : plugin.config.getConfigurationSection("Items").getKeys(false)) {
-                    if (ChatColor.translateAlternateColorCodes('&',
-                            plugin.config.getString("Items." + string + ".Name")).equals(meta.getDisplayName())) {
-                        event.setCancelled(true);
-                    }
-                }
-            }
-        }
-    }
+}
 
